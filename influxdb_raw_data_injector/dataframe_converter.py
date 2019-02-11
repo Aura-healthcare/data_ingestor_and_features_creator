ac7b174fc56f4c9cfc781d34d665363c8ad01454
robinchampseix <robin.champseix@gmail.com>
2019-02-11 22:43:14 +1100

    refactoring by adding variables as parameters in multiple files

--- influxdb_raw_data_injector/dataframe_converter.py
+++ influxdb_raw_data_injector/dataframe_converter.py
@@ -4,6 +4,18 @@
 
 import pandas as pd
 
+# Variables from websocket JSON files
+RRI_MEASUREMENT_NAME = "RrInterval"
+TIMESTAMP_DATAFRAME_COL_NAME = "timestamp"
+X_ACM_DATAFRAME_COL_NAME = "x_acm"
+Y_ACM_DATAFRAME_COL_NAME = "y_acm"
+Z_ACM_DATAFRAME_COL_NAME = "z_acm"
+X_GYRO_DATAFRAME_COL_NAME = "x_gyro"
+Y_GYRO_DATAFRAME_COL_NAME = "y_gyro"
+Z_GYRO_DATAFRAME_COL_NAME = "z_gyro"
+SENSIBILITY_DATAFRAME_COL_NAME = "sensibility"
+JSON_DATA_FIELD_NAME = "data"
+
 def convert_acm_json_to_df(acm_json: dict) -> pd.DataFrame:
     """
     Function converting accelerometer JSON data to pandas Dataframe.
@@ -17,13 +29,18 @@ def convert_acm_json_to_df(acm_json: dict) -> pd.DataFrame:
     df_to_write - Dataframe to write in influxDB
     """
     # Extract values to write to InfluxDB
-    extracted_data_from_json_file = list(map(lambda x: x.split(" "), acm_json["data"]))
+    extracted_data_from_json_file = list(map(lambda x: x.split(" "), acm_json[JSON_DATA_FIELD_NAME]))
 
-    columns = ["timestamp", "x_acm", "y_acm", "z_acm", "sensibility"]
-    df_to_write = pd.DataFrame(extracted_data_from_json_file, columns=columns).set_index("timestamp")
+    columns = [TIMESTAMP_DATAFRAME_COL_NAME, X_ACM_DATAFRAME_COL_NAME, Y_ACM_DATAFRAME_COL_NAME,
+               Z_ACM_DATAFRAME_COL_NAME, SENSIBILITY_DATAFRAME_COL_NAME]
+    df_to_write = pd.DataFrame(extracted_data_from_json_file, columns=columns).set_index(TIMESTAMP_DATAFRAME_COL_NAME)
 
     # Convert string to numeric values
-    df_to_write[["x_acm", "y_acm", "z_acm"]] = df_to_write[["x_acm", "y_acm", "z_acm"]].apply(pd.to_numeric)
+    df_to_write[[X_ACM_DATAFRAME_COL_NAME,
+                 Y_ACM_DATAFRAME_COL_NAME,
+                 Z_ACM_DATAFRAME_COL_NAME]] = df_to_write[[X_ACM_DATAFRAME_COL_NAME,
+                                                           Y_ACM_DATAFRAME_COL_NAME,
+                                                           Z_ACM_DATAFRAME_COL_NAME]].apply(pd.to_numeric)
     # Convert index to datetime index
     df_to_write.index = pd.to_datetime(df_to_write.index)
     return df_to_write
@@ -42,13 +59,13 @@ def convert_rri_json_to_df(rri_json) -> pd.DataFrame:
     df_to_write - Dataframe to write in influxDB
     """
     # Extract values to write to InfluxDB
-    extracted_data_from_json_file = list(map(lambda x: x.split(" "), rri_json["data"]))
+    extracted_data_from_json_file = list(map(lambda x: x.split(" "), rri_json[JSON_DATA_FIELD_NAME]))
 
-    columns = ["timestamp", "RrInterval"]
-    df_to_write = pd.DataFrame(extracted_data_from_json_file, columns=columns).set_index("timestamp")
+    columns = [TIMESTAMP_DATAFRAME_COL_NAME, RRI_MEASUREMENT_NAME]
+    df_to_write = pd.DataFrame(extracted_data_from_json_file, columns=columns).set_index(TIMESTAMP_DATAFRAME_COL_NAME)
 
     # Convert string to numeric values
-    df_to_write["RrInterval"] = df_to_write["RrInterval"].apply(pd.to_numeric)
+    df_to_write[RRI_MEASUREMENT_NAME] = df_to_write[RRI_MEASUREMENT_NAME].apply(pd.to_numeric)
     # Convert index to datetime index
     df_to_write.index = pd.to_datetime(df_to_write.index)
     return df_to_write
@@ -67,13 +84,17 @@ def convert_gyro_json_to_df(gyro_json) -> pd.DataFrame:
     df_to_write - Dataframe to write in influxDB
     """
     # Extract values to write to InfluxDB
-    extracted_data_from_json_file = list(map(lambda x: x.split(" "), gyro_json["data"]))
+    extracted_data_from_json_file = list(map(lambda x: x.split(" "), gyro_json[JSON_DATA_FIELD_NAME]))
 
-    columns = ["timestamp", "x_gyro", "y_gyro", "z_gyro"]
-    df_to_write = pd.DataFrame(extracted_data_from_json_file, columns=columns).set_index("timestamp")
+    columns = [TIMESTAMP_DATAFRAME_COL_NAME, X_GYRO_DATAFRAME_COL_NAME, Y_GYRO_DATAFRAME_COL_NAME, Y_GYRO_DATAFRAME_COL_NAME]
+    df_to_write = pd.DataFrame(extracted_data_from_json_file, columns=columns).set_index(TIMESTAMP_DATAFRAME_COL_NAME)
 
     # Convert string to numeric values
-    df_to_write[["x_gyro", "y_gyro", "z_gyro"]] = df_to_write[["x_gyro", "y_gyro", "z_gyro"]].apply(pd.to_numeric)
+    df_to_write[[X_GYRO_DATAFRAME_COL_NAME,
+                 Y_GYRO_DATAFRAME_COL_NAME,
+                 Z_GYRO_DATAFRAME_COL_NAME]] = df_to_write[[X_GYRO_DATAFRAME_COL_NAME,
+                                                            Y_GYRO_DATAFRAME_COL_NAME,
+                                                            Z_GYRO_DATAFRAME_COL_NAME]].apply(pd.to_numeric)
     # Convert index to datetime index
     df_to_write.index = pd.to_datetime(df_to_write.index)
     return df_to_write
